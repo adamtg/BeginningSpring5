9b9ec93d93fc87b07db5cfeca5accab486186123
Adam Grossman <adamtg@devitron.com>
2022-05-07 15:41:41 -0400

    First

---
+++ bsg5/src/test/java/com/devitron/bsg5/Bsg5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devitron.bsg5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bsg5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
